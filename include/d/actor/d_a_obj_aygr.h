2a01a17dbe05b2cc5607b0a7b8d2978450a32b23
garrettsparks <garrettsparks@users.noreply.github.com>
2026-05-22 18:25:57 -0700

    d_a_obj_aygr OK

--- include/d/actor/d_a_obj_aygr.h
+++ include/d/actor/d_a_obj_aygr.h
@@ -2,12 +2,14 @@
 #define D_A_OBJ_AYGR_H
 
 #include "d/d_bg_s_movebg_actor.h"
+#include "d/d_a_obj.h"                       // for daObj::PrmAbstract
+#include "SSystem/SComponent/c_phase.h"      // for request_of_phase_process_class
 
 namespace daObjAygr {
     class Act_c : public dBgS_MoveBgActor {
     public:
-        void prm_get_mdl() const {}
-    
+        u8 prm_get_mdl() const { return daObj::PrmAbstract(this, PRM_MDL_W, PRM_MDL_S); }
+
         virtual BOOL CreateHeap();
         virtual BOOL Create();
         cPhs_State Mthd_Create();
@@ -17,9 +19,22 @@ namespace daObjAygr {
         void init_mtx();
         virtual BOOL Execute(Mtx**);
         virtual BOOL Draw();
-    
+
+        enum Prm_e {
+            PRM_MDL_W = 1,    // width=1, from asm (li r4, 0x1)
+            PRM_MDL_S = 0,    // shift=0, from asm (li r5, 0x0)
+        };
+
+        static Mtx M_tmp_mtx;
+        static const char M_arcname[];
+
     public:
-        /* Place member variables here */
+        /* 0x2C8 */ request_of_phase_process_class mPhs;
+        /* 0x2D0 */ J3DModel* mpModel;
+        /* 0x2D4 */ J3DModel* field_0x2d4;
+        /* 0x2D8 */ dBgW* field_0x2d8;
+        /* 0x2DC */ Mtx field_0x2dc;        // 48 bytes, ends at 0x30C
+        /* 0x30C */ u8 field_0x30c;
     };
 };
 
--- src/d/actor/d_a_obj_aygr.cpp
+++ src/d/actor/d_a_obj_aygr.cpp
@@ -7,50 +7,144 @@
 #include "d/actor/d_a_obj_aygr.h"
 #include "d/d_procname.h"
 #include "d/d_priority.h"
+#include "d/res/res_aygr.h"
+#include "m_Do/m_Do_ext.h"
+
+Mtx daObjAygr::Act_c::M_tmp_mtx;
+const char daObjAygr::Act_c::M_arcname[] = "Aygr";
 
 /* 00000078-000002A4       .text CreateHeap__Q29daObjAygr5Act_cFv */
 BOOL daObjAygr::Act_c::CreateHeap() {
-    /* Nonmatching */
+    J3DModelData* model_data_yagura = (J3DModelData*)dComIfG_getObjectRes(M_arcname, AYGR_BDL_AYGR);
+    JUT_ASSERT(0x50, model_data_yagura != NULL);
+    mpModel = mDoExt_J3DModel__create(model_data_yagura, 0, 0x11020203);
+    if (mpModel == NULL) {
+        return FALSE;
+    }
+    if (prm_get_mdl()) {
+        J3DModelData* model_data_hashigo = (J3DModelData*)dComIfG_getObjectRes(M_arcname, AYGR_BDL_AYGRH);
+        JUT_ASSERT(0x59, model_data_hashigo != NULL);
+        field_0x2d4 = mDoExt_J3DModel__create(model_data_hashigo, 0, 0x11020203);
+        if (field_0x2d4 == NULL) {
+            return FALSE;
+        }
+        BOOL ok = TRUE;
+        mDoMtx_stack_c::transS(current.pos);
+        mDoMtx_stack_c::YrotM(shape_angle.y);
+        mDoMtx_stack_c::scaleM(scale);
+        cMtx_copy(mDoMtx_stack_c::get(), field_0x2dc);
+        field_0x2d8 = new dBgW();
+        if (field_0x2d8 != NULL) {
+            cBgD_t* pData = (cBgD_t*)dComIfG_getObjectRes(M_arcname, AYGR_DZB_AYGRH);
+            if (field_0x2d8->Set(pData, 1, &field_0x2dc) == 0) {
+                goto ok_set;
+            }
+        }
+        ok = FALSE;
+    ok_set:
+        if (ok != TRUE) return FALSE;
+        field_0x30c = 1;
+    } else {
+        field_0x2d4 = NULL;
+        field_0x2d8 = NULL;
+        field_0x30c = 0;
+    }
+    return TRUE;
 }
 
 /* 000002A4-00000310       .text Create__Q29daObjAygr5Act_cFv */
 BOOL daObjAygr::Act_c::Create() {
-    /* Nonmatching */
+    cullMtx = mpModel->getBaseTRMtx();
+    init_mtx();
+    fopAcM_setCullSizeBox(this, -500.0f, -100.0f, -500.0f, 500.0f, 4000.0f, 500.0f);
+    return TRUE;
 }
 
 /* 00000310-000004D4       .text Mthd_Create__Q29daObjAygr5Act_cFv */
 cPhs_State daObjAygr::Act_c::Mthd_Create() {
-    /* Nonmatching */
+    cPhs_State phase_state;
+    fopAcM_ct(this, Act_c);
+
+    field_0x30c = 0;
+
+    phase_state = dComIfG_resLoad(&mPhs, M_arcname);
+    if (phase_state == cPhs_COMPLEATE_e) {
+        phase_state = MoveBGCreate(M_arcname, AYGR_DZB_AYGR, NULL, 0x85F0);
+
+        if (mpBgW != NULL && mpBgW->ChkUsed()) {
+            mpBgW->Move();
+            mpBgW->SetLock();
+        }
+
+        if (prm_get_mdl()) {
+            dComIfG_Bgsp()->Regist(field_0x2d8, this);
+            if (field_0x2d8 != NULL && field_0x2d8->ChkUsed()) {
+                field_0x2d8->Move();
+                field_0x2d8->SetLock();
+            }
+        }
+
+        JUT_ASSERT(0xb7, (phase_state == cPhs_COMPLEATE_e) || (phase_state == cPhs_ERROR_e));
+    }
+    return phase_state;
 }
 
 /* 000004D4-000004DC       .text Delete__Q29daObjAygr5Act_cFv */
 BOOL daObjAygr::Act_c::Delete() {
-    /* Nonmatching */
+    return TRUE;
 }
 
 /* 000004DC-0000054C       .text Mthd_Delete__Q29daObjAygr5Act_cFv */
 BOOL daObjAygr::Act_c::Mthd_Delete() {
-    /* Nonmatching */
+    if (field_0x30c) {
+        dComIfG_Bgsp()->Release(field_0x2d8);
+    }
+    BOOL ret = MoveBGDelete();
+    dComIfG_resDelete(&mPhs, M_arcname);
+    return ret;
 }
 
 /* 0000054C-000005F8       .text set_mtx__Q29daObjAygr5Act_cFv */
 void daObjAygr::Act_c::set_mtx() {
-    /* Nonmatching */
+    mDoMtx_stack_c::transS(current.pos.x, current.pos.y, current.pos.z);
+    mDoMtx_stack_c::ZXYrotM(shape_angle.x, shape_angle.y, shape_angle.z);
+    mpModel->setBaseTRMtx(mDoMtx_stack_c::get());
+    if (prm_get_mdl()) {
+        field_0x2d4->setBaseTRMtx(mDoMtx_stack_c::get());
+    }
+    cMtx_copy(mDoMtx_stack_c::get(), M_tmp_mtx);
 }
 
 /* 000005F8-00000674       .text init_mtx__Q29daObjAygr5Act_cFv */
 void daObjAygr::Act_c::init_mtx() {
-    /* Nonmatching */
+    mpModel->setBaseScale(scale);
+    if (prm_get_mdl()) {
+        field_0x2d4->setBaseScale(scale);
+    }
+    set_mtx();
 }
 
 /* 00000674-000006B0       .text Execute__Q29daObjAygr5Act_cFPPA3_A4_f */
-BOOL daObjAygr::Act_c::Execute(Mtx**) {
-    /* Nonmatching */
+BOOL daObjAygr::Act_c::Execute(Mtx** i_pMtx) {
+    set_mtx();
+    *i_pMtx = &M_tmp_mtx;
+    return TRUE;
 }
 
 /* 000006B0-0000079C       .text Draw__Q29daObjAygr5Act_cFv */
 BOOL daObjAygr::Act_c::Draw() {
-    /* Nonmatching */
+    g_env_light.settingTevStruct(TEV_TYPE_BG0, &current.pos, &tevStr);
+    g_env_light.setLightTevColorType(mpModel, &tevStr);
+    if (prm_get_mdl()) {
+        g_env_light.setLightTevColorType(field_0x2d4, &tevStr);
+    }
+    dComIfGd_setListBG();
+    mDoExt_modelUpdateDL(mpModel);
+    if (prm_get_mdl()) {
+        mDoExt_modelUpdateDL(field_0x2d4);
+    }
+    dComIfGd_setList();
+    return TRUE;
 }
 
 namespace daObjAygr {
